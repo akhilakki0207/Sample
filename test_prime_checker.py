cf56657f517628e25e9443eaff06fc30fdca06e6
Akhil Akki <78601774+akhilakki0207@users.noreply.github.com>
2025-04-09 13:05:47 -0400

    Update test_prime_checker.py
    2 test cases only

--- test_prime_checker.py
+++ test_prime_checker.py
@@ -6,13 +6,10 @@ class TestPrimeChecker(unittest.TestCase):
     def test_primes(self):
         self.assertTrue(is_prime(2))
         self.assertTrue(is_prime(3))
-        self.assertTrue(is_prime(13))
 
     def test_non_primes(self):
         self.assertFalse(is_prime(0))
         self.assertFalse(is_prime(1))
-        self.assertFalse(is_prime(4))
-        self.assertFalse(is_prime(100))
 
 if __name__ == "__main__":
     unittest.main()
